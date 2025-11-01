c99a883e24aaafce2f68fb02aba63fed1ec69ff2
Naveed <46922100+nedanwr@users.noreply.github.com>
2025-11-02 01:48:06 +0800

    (Fix)[api]: update `IdType` to replace 'id' with 'link' for improved clarity in ID generation

--- packages/api/src/lib/id.ts
+++ packages/api/src/lib/id.ts
@@ -1,5 +1,5 @@
 import { nanoid } from "../utils/nanoid";
 
-type IdType = "project" | "user" | "env" | "settings" | "magic" | "id";
+type IdType = "project" | "user" | "env" | "settings" | "magic" | "link";
 
 export const generateId = (prefix: IdType) => `${prefix}_${nanoid(12)}`;
